520a6d5537e14b23a35fe7774d42dcf0503c8530
michaelgetachew-abebe <101546624+michaelgetachew-abebe@users.noreply.github.com>
2022-04-08 19:08:09 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i;
-	i=97;
-	while(i<=122)
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (i == 101 || i == 113)
-		{
-			continue;
-		}
-		else
-		{
-			putchar(i);
-		}
-		i++;
+		if (c != 'e' && c != 'q')
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
