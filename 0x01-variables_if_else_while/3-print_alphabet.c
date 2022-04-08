e357700e5e50afa5c915060e70864b7f882c8b9f
michaelgetachew-abebe <nixongetachew@gmail.com>
2022-04-08 04:54:57 -0700

    Modification 4

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	i=97;
+	while(i<=122)
+	{
+		putchar(i);
+		i++;
+	}
+	i=65;
+	while(i<91)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n);
+	return (0);
+}
