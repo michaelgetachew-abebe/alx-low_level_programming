d4a62e863cf080c1783925ae4c1f6a285565b8b9
michaelgetachew-abebe <nixongetachew@gmail.com>
2022-04-08 07:23:13 -0700

    Modification

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	i = 48;
+	while(i<=57)
+	{
+		putchar(i);
+		i++;
+	}
+	i=97;
+	while(i<=102)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
