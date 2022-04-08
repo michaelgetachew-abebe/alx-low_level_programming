41b6ced63e639a4ba940dcece358388ebfc18f11
michaelgetachew-abebe <101546624+michaelgetachew-abebe@users.noreply.github.com>
2022-04-08 19:04:36 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,10 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
 		putchar(c);
-	for (c = 'A'; c <= 'Z'; c++)
-		putchar(c);
+	}
 	putchar('\n');
 	return (0);
 }
