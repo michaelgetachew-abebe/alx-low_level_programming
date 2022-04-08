51b3c7704b21db2a5bc28ce94693b6a81acc934d
michaelgetachew-abebe <101546624+michaelgetachew-abebe@users.noreply.github.com>
2022-04-08 19:11:46 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i;
-	i = 48;
-	while(i<=57)
+	char c = 'a';
+	int num;
+
+	for (num = 0; num < 16; num++)
 	{
-		putchar(i);
-		i++;
-	}
-	i=97;
-	while(i<=102)
-	{
-		putchar(i);
-		i++;
+		if (num < 10)
+		{
+			putchar(num + '0');
+		} else
+		{
+			putchar(c);
+			c++;
+		}
 	}
 	putchar('\n');
 	return (0);
