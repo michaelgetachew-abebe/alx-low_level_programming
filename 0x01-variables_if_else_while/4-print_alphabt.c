4a18771c41afe9315cd695aca19ed69ca56be881
michaelgetachew-abebe <nixongetachew@gmail.com>
2022-04-08 05:14:33 -0700

    Modificattion 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	i=97;
+	while(i<=122)
+	{
+		if (i == 101 || i == 113)
+		{
+			continue;
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
