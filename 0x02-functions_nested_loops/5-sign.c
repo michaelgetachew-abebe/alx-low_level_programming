aa54ea53b2720ac67f46b806fa40db2bb610fd96
michaelgetachew-abebe <nixongetachew@gmail.com>
2022-04-12 06:51:15 -0700

    Modification 1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,10 +8,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		return (1);
+		_putchar ('+');
+	}
 	else if (n < 0)
+	{
+		_putchar ('0');
 		return (-1);
+	}
 	else
+	{
+		_putchar ('-');
 		return (0);
+	}
 }
-
