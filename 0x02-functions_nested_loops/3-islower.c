d159b8cccca1e4e7f990f61221849a776dcdbd2a
michaelgetachew-abebe <nixongetachew@gmail.com>
2022-04-12 06:23:31 -0700

    Implementation 1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,12 @@
 /**
  * _islower - checks for lowercase character
  *@c: single letter input
- * Return: Always 0 (Success)
+ * Return: Always 1 OR 0 (Success)
  */
 int _islower(int c)
 {
-	int r;
-
 	if (c > 97)
 		return (1);
 	else
 		return (0);
 }
-
