e11e59bd07d3ce98f55f1eeb6f3d4682e9028076
michaelgetachew-abebe <nixongetachew@gmail.com>
2022-04-12 06:15:44 -0700

    Change 4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	int r;
+
+	if (c > 97)
+	{
+		r=1;
+	}
+	else
+	{
+		r=0;
+	}
