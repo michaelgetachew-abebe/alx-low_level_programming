8a74219c4c63263f2a2e95f0f9d32a123993b553
michaelgetachew-abebe <nixongetachew@gmail.com>
2022-04-12 08:23:04 -0700

    Done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: integer input
- * Returns: value of the last digit
+ * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
