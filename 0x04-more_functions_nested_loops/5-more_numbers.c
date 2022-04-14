785538d6935b84aa7909f9344220e4e8d5aaf251
michaelgetachew-abebe <nixongetachew@gmail.com>
2022-04-14 03:14:54 -0700

    Modification 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,2 +1,20 @@
 #include "main.h"
 
+/**
+ * more_numbers - prints numbers from 0 to 14 
+ * 10 times
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar('j');
+		}
+		_putchar('\n');
+	}
+}
