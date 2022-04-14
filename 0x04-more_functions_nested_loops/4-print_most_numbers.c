60131e8b5744abd1306344d56d13f706314a06c0
michaelgetachew-abebe <nixongetachew@gmail.com>
2022-04-14 02:49:53 -0700

    Modification 5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints all numbers from 0 to 9
+ * except 2 and 4;
+ * Retuen: 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if ( (i == 50) || (i == 52))
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+}
