4977e0b33272e668b5238d7c3c5548f6c0c92d82
michaelgetachew-abebe <nixongetachew@gmail.com>
2022-05-30 12:03:27 -0700

    fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,42 @@
 #include "main.h"
-
 /**
- * read_textfile - reads a text file
- * @filename: path of the file to read
- * @letters: number of letters to read and print
- * Return: number of letters readed.
- */
+* read_textfile - check the code for Holberton School students.
+* @filename: file to read and write
+* @letters: number of letters to read and write.
+* Return: letters printed
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	int count, fd_o, fd_r;
+	ssize_t nletters;
+	int file;
+	char *text;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-
-	/* Open file and get file descriptor */
-	fd_o = open(filename, O_RDONLY);
-	if (fd_o == -1)
+	text = malloc(sizeof(char) * letters + 1);
+	if (text == NULL)
 		return (0);
-
-	/* allocate memory in buffer to read contents of fd */
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
-	/* read contents of fd */
-	fd_r = read(fd_o, buffer, letters);
-	if (fd_r == 0)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 	{
-		free(buffer);
-		close(fd_o);
+		free(text);
 		return (0);
 	}
-
-	/* write to standard output */
-	count = write(STDOUT_FILENO, buffer, fd_r);
-	if (count == -1)
+	nletters = read(file, text, sizeof(char) * letters);
+	if (nletters == -1)
 	{
-		free(buffer);
-		close(fd_r);
-		close(fd_o);
+		free(text);
+		close(file);
 		return (0);
 	}
-	close(fd_r);
-	close(fd_o);
-
-	return (count);
+	nletters = write(STDOUT_FILENO, text, nletters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	free(text);
+	close(file);
+	return (nletters);
 }
